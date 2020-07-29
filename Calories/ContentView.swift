6e8e874bd86521e77ad747da9946b289b4d4e865
Matteo Manferdini <matteo@purecreek.com>
2020-07-29 15:53:22 +0200

    Initial Commit

---
+++ Calories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calories
+//
+//  Created by Matteo Manferdini on 29/07/2020.
+//  Copyright © 2020 Matteo Manferdini. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
